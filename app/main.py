6bcadfcdd4223777fe03f75a9647be1180ac5f53
EpocDotFr <contact@epoc.fr>
2023-11-16 18:11:00 +0100

    Improvements

--- app/main.py
+++ app/main.py
@@ -1,46 +1,60 @@
 from typing import BinaryIO
 import argparse
 import struct
+import os
 
 
-class SQLite:
-    database: BinaryIO
+class SQLiteFile:
+    f: BinaryIO
 
-    def __init__(self, database: BinaryIO):
-        self.database = database
-
-    def unpack(self, fmt, size=1):
-        return struct.unpack(
-            f'>{fmt}',
-            self.database.read(size)
-        )
+    def __init__(self, f: BinaryIO):
+        self.f = f
 
     def exec(self, command: str) -> None:
         if command[0] == '.': # Dot command
             if command == '.dbinfo':
-                self.database.seek(16)
+                self.move(16)
 
-                page_size = self.unpack('H', 2)[0]
-
-                print(f'database page size: {page_size}')
+                print(f'database page size: {self.read_uint16()}')
                 print(f'number of tables: TODO')
             elif command == '.tables':
                 pass
             else:
-                raise ValueError('Unknown command')
+                raise ValueError('Unknown dot command')
         else: # SQL query
             pass
+
+    def unpack(self, fmt: str, size: int = 1):
+        ret = struct.unpack(
+            f'>{fmt}',
+            self.read_bytes(size)
+        )
+
+        return ret[0] if len(ret) == 1 else ret
+
+    def read_uint16(self):
+        return self.unpack('H', 2)
+
+    def read_bytes(self, size: int):
+        return self.f.read(size)
+
+    def read_byte(self):
+        return self.read_bytes(1)
+
+    def move(self, offset: int):
+        self.f.seek(offset, os.SEEK_CUR)
 
 
 def main() -> None:
     arg_parser = argparse.ArgumentParser()
-    arg_parser.add_argument('database', type=argparse.FileType('rb'))
+    arg_parser.add_argument('filename')
     arg_parser.add_argument('command')
 
     args = arg_parser.parse_args()
 
-    db = SQLite(args.database)
-    db.exec(args.command)
+    with open(args.filename, 'rb') as f:
+        sqlite = SQLiteFile(f)
+        sqlite.exec(args.command)
 
 
 if __name__ == '__main__':
